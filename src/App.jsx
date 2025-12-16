313f3cbed21a2eb8b66e0f49f1f753eb8e6648e0
Tusempapi999 <atapiapers@gmail.com>
2025-12-16 12:27:49 -0600

    Version_Base

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+    setUsers(response.data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <div class="flex h-screen items-center justify-center bg-gray-100"> 
+          <ul class="max-w-md space-y-1 text-body list-disc list-inside">
+            {users?.map((user) => (<li key={user.id}>{user.name}</li>))}
+          </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
